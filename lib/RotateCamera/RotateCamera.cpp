510aa0fdba47a115a50728c4bd113c5ac2a49913
Per Öberg <per.oberg@cassun.se>
2013-05-09 17:12:28 +0200

    Deleted old lib CameraSetting

--- lib/RotateCamera/RotateCamera.cpp
+++ lib/RotateCamera/RotateCamera.cpp
@@ -2,14 +2,15 @@
 
 //Public
 void RotateCamera::move(float panCordinate, float tiltCordinate){
-	this->pan(panCordinate);
 	this->tilt(tiltCordinate);
+	this->pan(panCordinate);
+	
 }
 
 //Private
 void RotateCamera::pan(float cordinate){
 	//Calc degs to move
-	float currentCordinate = (float)_sensorPan.readDeg();
+	float currentCordinate = _sensorPan.readDeg();
 	float degsToMove = cordinate - currentCordinate;
 	//Move camera
 	_stepperPan.run(degsToMove * _gearRatio, _speed);
@@ -17,7 +18,7 @@ void RotateCamera::pan(float cordinate){
 
 void RotateCamera::tilt(float cordinate){
 	//Calc degs to move
-	float currentCordinate = (float)_sensorTilt.readDeg();
+	float currentCordinate = _sensorTilt.readDeg();
 	float degsToMove = cordinate - currentCordinate;
 	//Move camera
 	_stepperTilt.run(degsToMove * _gearRatio, _speed);
--- src/foto360.ino
+++ src/foto360.ino
@@ -1,18 +1,36 @@
 #include "Arduino.h"
 #include "RotateCamera.h"
-
-
+#include "MainDisplay.h"
+#include "BatterySensor.h"
 //int tiltReadAnalogPin, int tiltStepperDirPin, int tiltStepperStepPin,
 //int panReadAnalogPin, int panStepperDirPin, int panStepperStepPin
-RotateCamera camera(2, 3, 4, 6, 7, 8);
+//RotateCamera camera(2, 26, 9, 0, 25, 10);
+//int rs, int enable,int d4, int d5, int d6, int d7)
+MainDisplay display(22,13,12,23,11,24);
+
+BatterySensor battery(8);
+
 
 void setup() {
-  
+	Serial2.begin(115200);
+	pinMode(7,OUTPUT);
 }
 
 void loop() {
+	
 
-	camera.move(30,20);
-	delay(5000);
+	if(Serial2.available()){
+		char val = Serial2.read();
+		if(val = 'S'){
+			digitalWrite(7,HIGH);
+		}
+		delay(1500);
+		digitalWrite(7,LOW);
+	}
+	int batPercenage = battery.readPercentage();
+
+	display.pauseScreen(12,13,batPercenage,213);
+	delay(2000);
+	
   
 }
