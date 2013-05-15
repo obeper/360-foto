99eb7714be14592446ff5e9e86ade5b25c966548
Per Öberg <per.oberg@cassun.se>
2013-05-16 00:32:34 +0200

    Addes some things in PanoramaSettings

--- lib/PanoramaSettings/PanoramaSettings.cpp
+++ lib/PanoramaSettings/PanoramaSettings.cpp
@@ -13,17 +13,37 @@ PanoramaSettings::PanoramaSettings(float focal, float sensorH, float sensorV, fl
 	_sensorVertical =14.9; 
 	_overlap = 1.2;
 	*/
+	this->calcNrOfPictures();
 }
 
-void PanoramaSettings::calc(){
-float fovHorizontal = (2.0 * atan(_sensorHorizontal / (2.0 * _focalLength))) * (180.0/3.14);
-float fovVertical =   (2.0 * atan(_sensorVertical / (2.0 * _focalLength))) * (180.0/3.14);
+int PanoramaSettings::getNrOfPictures(){
+	return _totalNrOfPictures;
+}
+void PanoramaSettings::setFocalLength(float focal){
+	_focalLength = focal;
+	this->calcNrOfPictures();
+}
+float PanoramaSettings::calcPanCordinate(int picNr){
+	if(picNr == 0) return 0;
+	return picNr*(_verticalAngle/_verticalPics);
+	//FEEEEL
+} 
+float PanoramaSettings::calcTiltCordinate(int picNr){
+	if(picNr == 0) return 0;
+	return picNr*(360/_horizontalPics);
+	//FEEEL
+}
 
-int horizontalPics = (int)(((360/fovHorizontal)*_overlap)+0.5);
-float verticalAngle = 180 - min(fovHorizontal,fovVertical);
-int verticalPics = (int)((verticalAngle/fovVertical)*_overlap + 0.5);
 
-int totalNrOfPics = 2.0 + horizontalPics*verticalPics;
+void PanoramaSettings::calcNrOfPictures(){
+	float fovHorizontal = (2.0 * atan(_sensorHorizontal / (2.0 * _focalLength))) * (180.0/3.14);
+	float fovVertical =   (2.0 * atan(_sensorVertical / (2.0 * _focalLength))) * (180.0/3.14);
 
+	_horizontalPics = (int)(((360/fovHorizontal)*_overlap)+0.5);
+	_verticalAngle = 180 - min(fovHorizontal,fovVertical);
+	_verticalPics = (int)((_verticalAngle/fovVertical)*_overlap + 0.5);
+
+	_totalNrOfPictures = 2.0 + _horizontalPics*_verticalPics;
+	
 
 }
--- lib/PanoramaSettings/PanoramaSettings.h
+++ lib/PanoramaSettings/PanoramaSettings.h
@@ -11,14 +11,22 @@ class PanoramaSettings
 
   	//CONSTRUCT
     PanoramaSettings(float focal, float sensorH, float sensorV, float overlap);
-
-    
+    int getNrOfPictures();
+    void setFocalLength(float focal);
+    float calcPanCordinate(int picNr);
+    float calcTiltCordinate(int picNr);
   private:
-  	float _focalLength; 
-	float _sensorHorizontal; 
-	float _sensorVertical; 
-	float _overlap;
-	void calc();
+  	float 	_focalLength; 
+	float 	_sensorHorizontal; 
+	float 	_sensorVertical; 
+	float 	_overlap;
+
+	int	  	_totalNrOfPictures;
+	int	 	_horizontalPics;
+	float	_verticalAngle;
+	int		_verticalPics;
+	void calcNrOfPictures();
+	
    
 };
 
